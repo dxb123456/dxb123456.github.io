ed1a9a2a122cefbc43cb9e935eedee4022cb1d70
dxb123456 <303798510@qq.com>
2016-07-23 00:11:16 +0800

    tengxunzuopin

---
+++ js/0.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Administrator on 2016/7/21.
+ */
+
+    function f0(){
+        $("#div00 img").eq(1).css({width:285,height:76,left:340,top:250,opacity:1});
+        $("#div00 img").eq(0).css({width:32,height:20,left:310,top:242,opacity:1});
+        $("#div00 img").eq(2).css({width:72,height:72,left:365,top:330,opacity:1})
+        $("#div00 img").eq(3).css({width:216,height:75,left:442,top:323,opacity:1})
+        $("#div00 img").eq(4).css({width:32,height:20,left:658,top:308,opacity:1})
+    }
+
+
+/*#div00 img:nth-of-type(1){left:310px;top:242px;width: 32px;height: 20px;}*/
+/*#div00 img:nth-of-type(2){left:340px;top:250px;width: 285px;height: 76px;}*/
+/*#div00 img:nth-of-type(3){left:365px;top:330px;width: 72px;height: 72px;}*/
+/*#div00 img:nth-of-type(4){left:442px;top:323px;width: 216px;height: 75px;}*/
+/*#div00 img:nth-of-type(5){left:658px;top:308px;width: 32px;height: 20px;}*/
